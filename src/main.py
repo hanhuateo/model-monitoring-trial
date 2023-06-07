72ed847850cb7fde0e45b2a25848c65712c05374
Han Hua Teo <hanhuateo@gmail.com>
2023-06-07 11:04:56 +0800

    output various monitoring reports

--- src/main.py
+++ src/main.py
@@ -2,6 +2,36 @@ import numpy as pandas
 import seaborn as sns
 import pandas as pd
 from joblib import load
+from monitoring.evidently import *
 
 RF_clf = load('../data/models/RF_clf.joblib')
-current_data = pd.read_csv('../data/split_data/train_dataset')
+reference = pd.read_csv('../data/split_data/train_dataset.csv')
+current = pd.read_csv('../data/split_data/test_dataset.csv')
+
+# see if got drift between reference and current data
+# also run the current data through the random forest classifier and compare the
+# evaluation curves and performance metrics
+
+report = Report(metrics=[])
+report = setDataDriftPreset(report)
+report.run(current_data=current, reference_data=reference)
+report.save_html('./monitoring/reports/DataDriftReport.html')
+
+colName1 = 'DailyRate'
+report = setColumnMetric(report, colName1)
+report.run(current_data=current, reference_data=reference)
+report.save_html('./monitoring/reports/ColumnMetricReport_{}.html'.format(colName1))
+
+# colName2 = 'Department'
+# report = setColumnMetric(report, colName2)
+# report.run(current_data=current, reference_data=reference)
+# report.save_html('./monitoring/reports/ColumnMetricReport_{}.html'.format(colName2))
+
+report = setTests(report)
+report.run(current_data=current, reference_data=reference)
+report.save_html('./monitoring/reports/TestsReport.html')
+
+report = setNoTargetTest(report)
+report.run(current_data=current, reference_data=reference)
+report.save_html('./monitoring/reports/NoTargetTestsReport.html')
+
