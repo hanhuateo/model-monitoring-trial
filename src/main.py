4c6e4714a555302c1a6266985cb9c25906398009
Han Hua Teo <hanhuateo@gmail.com>
2023-06-07 11:14:47 +0800

    same, generate html monitoring reports

--- src/main.py
+++ src/main.py
@@ -12,26 +12,28 @@ current = pd.read_csv('../data/split_data/test_dataset.csv')
 # also run the current data through the random forest classifier and compare the
 # evaluation curves and performance metrics
 
-report = Report(metrics=[])
-report = setDataDriftPreset(report)
-report.run(current_data=current, reference_data=reference)
-report.save_html('./monitoring/reports/DataDriftReport.html')
+# report = Report(metrics=[])
+# report = setDataDriftPreset(report)
+# report.run(current_data=current, reference_data=reference)
+# report.save_html('./monitoring/reports/DataDriftReport.html')
 
-colName1 = 'DailyRate'
-report = setColumnMetric(report, colName1)
-report.run(current_data=current, reference_data=reference)
-report.save_html('./monitoring/reports/ColumnMetricReport_{}.html'.format(colName1))
+# colName1 = 'DailyRate'
+# report = setColumnMetric(report, colName1)
+# report.run(current_data=current, reference_data=reference)
+# report.save_html('./monitoring/reports/ColumnMetricReport_{}.html'.format(colName1))
+# print(report.json)
 
 # colName2 = 'Department'
 # report = setColumnMetric(report, colName2)
 # report.run(current_data=current, reference_data=reference)
 # report.save_html('./monitoring/reports/ColumnMetricReport_{}.html'.format(colName2))
 
-report = setTests(report)
-report.run(current_data=current, reference_data=reference)
-report.save_html('./monitoring/reports/TestsReport.html')
+# report = setTests(report)
+# report.run(current_data=current, reference_data=reference)
+# report.save_html('./monitoring/reports/TestsReport.html')
+# print(report.json)
 
-report = setNoTargetTest(report)
-report.run(current_data=current, reference_data=reference)
-report.save_html('./monitoring/reports/NoTargetTestsReport.html')
-
+# report = setNoTargetTest(report)
+# report.run(current_data=current, reference_data=reference)
+# report.save_html('./monitoring/reports/NoTargetTestsReport.html')
+# print(report.json)
