ab652f3c511150960f3af051229aa72b456f6ee5
Han Hua Teo <hanhuateo@gmail.com>
2023-06-08 11:54:57 +0800

    saved processed X_test and X_train data

--- src/data_manipulation/preprocess.py
+++ src/data_manipulation/preprocess.py
@@ -10,6 +10,10 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 from sklearn.model_selection import train_test_split
 
+from scipy.sparse import save_npz
+
+from numpy import savetxt, save
+
 def split(df):
     X = df.drop(columns=['Attrition'])
     y = df['Attrition']
@@ -50,13 +54,24 @@ def train_preprocessing(X_train, X_test, y_train, y_test):
     # using make_column_transformer
     X_train_processed = preprocessor.fit_transform(X_train)
     print(f"Shape of X_train_processed after preprocessing: {X_train_processed.shape}")
+    print('type of X_train_processed : {}'.format(type(X_train_processed).__name__))
+    if (type(X_train_processed).__name__ == 'ndarray'):
+        savetxt('../data/processed/X_train_processed.csv', X_train_processed, delimiter=',')
+    else:
+        save_npz('../data/processed/X_train_processed.npz', X_train_processed)
 
     X_test_processed = preprocessor.transform(X_test)
     print(f"Shape of X_test_processed after preprocessing: {X_test_processed.shape}")
+    print('type of X_test_processed : {}'.format(type(X_test_processed).__name__))
+    if (type(X_test_processed).__name__ == 'ndarray'):
+        savetxt('../data/processed/X_test_processed.csv', X_test_processed, delimiter=',')
+    else:
+        save_npz('../data/processed/X_test_processed.npz', X_test_processed)
 
     LE = LabelEncoder()
     y_train_processed = LE.fit_transform(y_train)
     print(f"Shape of y_train_processed after preprocessing: {y_train_processed.shape}")
+    save('../data/processed/y_train_processed.npy', )
 
     y_test_processed = LE.transform(y_test)
     print(f"Shape of y_test_processed after preprocessing: {y_test_processed.shape}")
@@ -65,21 +80,3 @@ def train_preprocessing(X_train, X_test, y_train, y_test):
     print(f"y_train before preprocessing: {LE.inverse_transform(y_train_processed)}") 
 
     return X_train_processed,  X_test_processed, y_train_processed, y_test_processed
-
-def test_preprocessing(X):
-    # define categorical and numerical transformers
-    categorical_transformer = Pipeline(steps=[
-        # ('SimpleImputer', SimpleImputer(strategy='most_frequent')),
-        ('encoder', OneHotEncoder(drop=None))
-    ])
-
-    numerical_transformer = Pipeline(steps=[
-        # ('knnImputer', KNNImputer(n_neighbors=3, weights="uniform")),
-        ('scaler', StandardScaler())
-    ])
-    
-    #  dispatch object columns to the categorical_transformer and remaining columns to numerical_transformer
-    preprocessor = ColumnTransformer(transformers=[
-        ('categorical', categorical_transformer, make_column_selector(dtype_include="category")),
-        ('numerical', numerical_transformer, make_column_selector(dtype_exclude="category"))
-    ])
