3fad4a257e9b251d590d682b2b0907977cb38abf
Han Hua Teo <hanhuateo@gmail.com>
2023-06-08 14:55:04 +0800

    l

--- src/data_manipulation/understandings.py
+++ src/data_manipulation/understandings.py
@@ -1,5 +1,4 @@
 def object_to_category(df):
-    feature_names = df.drop(columns='Attrition').columns.to_list()
     nominal_features = df.drop(columns=['Attrition']).select_dtypes(include=['object']).columns.tolist()
     df[nominal_features] = df[nominal_features].astype('category')
     return df
