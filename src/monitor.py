ec3f4caf0ba276e31bdfb18e7ff5b8540077cb87
Han Hua Teo <hanhuateo@gmail.com>
2023-06-12 17:26:33 +0800

    created monitor.py

---
+++ src/monitor.py
@@ -0,0 +1,20 @@
+from monitoring.evidently import *
+import pandas as pd
+
+X_combined = pd.read_csv('../data/split_data/X_combined.csv')
+X_current = pd.read_csv('../data/split_data/X_current.csv')
+
+X_combined_processed = pd.read_csv('../data/processed/X_combined_processed.csv')
+X_current_processed = pd.read_csv('../data/processed/X_current_processed.csv')
+
+y_combined = pd.read_csv('../data/split_data/y_combined.csv')
+y_current = pd.read_csv('../data/split_data/y_current.csv')
+
+y_combined_processed = pd.read_csv('../data/processed/y_combined_processed.csv')
+y_current_processed = pd.read_csv('../data/processed/y_current_processed.csv')
+
+data_drift_report = Report(metrics=[
+    DataDriftPreset(),
+])
+data_drift_report.run(reference_data=X_combined, current_data=X_current)
+data_drift_report.save_html('./monitoring/reports/features_data_drift_report.html')
