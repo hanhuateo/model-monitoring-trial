e73700b639aa2d72490e5f206f4bc6f776586907
Han Hua Teo <hanhuateo@gmail.com>
2023-06-13 16:29:34 +0800

    13/06/23 1630h added data_quality_report, data_quality_test_suite, num_target_drift_report, to evaluate the outputs to see if it they are useful

--- src/monitor.py
+++ src/monitor.py
@@ -14,9 +14,42 @@ y_current = pd.read_csv('../data/split_data/y_current.csv')
 y_combined_processed = pd.read_csv('../data/processed/y_combined_processed.csv')
 y_current_processed = pd.read_csv('../data/processed/y_current_processed.csv')
 
+y_prediction = pd.read_csv('../data/predicted/y_prediction.csv')
+
 column_mapping = ColumnMapping()
 column_mapping.target = 'Attrition'
-
+column_mapping.prediction = 'Prediction'
+column_mapping.id = None
+column_mapping.categorical_features = ['Department',
+                                       'EducationField',
+                                       'Gender',
+                                       'JobRole',
+                                       'MaritalStatus',
+                                       'OverTime',
+                                       'BusinessTravel',
+                                       'Education',
+                                       'EnvironmentSatisfaction',
+                                       'JobInvolvement',
+                                       'JobLevel',
+                                       'JobSatisfaction',
+                                       'PerformanceRating',
+                                       'RelationshipSatisfaction',
+                                       'StockOptionLevel',
+                                       'WorkLifeBalance']
+column_mapping.numerical_features = ['Age', 
+                                     'DailyRate', 
+                                     'DistanceFromHome', 
+                                     'HourlyRate',
+                                     'MonthlyIncome',   
+                                     'MonthlyRate',
+                                     'NumCompaniesWorked',
+                                     'PercentSalaryHike',
+                                     'TotalWorkingYears',
+                                     'TrainingTimesLastYear',
+                                     'YearsAtCompany',
+                                     'YearsInCurrentRole',
+                                     'YearsSinceLastPromotion',
+                                     'YearsWithCurrManager']
 
 data_drift_report = Report(metrics=[
     # DataDriftPreset(cat_stattest='jensenshannon', num_stattest='ks'),
@@ -24,7 +57,7 @@ data_drift_report = Report(metrics=[
     DataDriftPreset(),
 ])
 
-data_drift_report.run(reference_data=X_combined, current_data=X_combined_processed)
+data_drift_report.run(reference_data=X_combined, current_data=X_current)
 data_drift_report.save_html('./monitoring/reports/data_drift_report.html')
 
 data_drift_test_suite = TestSuite(tests=[
@@ -33,3 +66,23 @@ data_drift_test_suite = TestSuite(tests=[
 
 data_drift_test_suite.run(reference_data=X_combined, current_data=X_current)
 data_drift_test_suite.save_html('./monitoring/reports/data_drift_test_suite.html')
+
+data_quality_report = Report(metrics=[
+    DataQualityPreset(),
+])
+
+data_quality_report.run(reference_data=X_combined, current_data=X_current)
+data_quality_report.save_html('./monitoring/reports/data_quality_report.html')
+
+data_quality_test_suite = TestSuite(tests=[
+    DataQualityTestPreset(),
+])
+
+data_quality_test_suite.run(reference_data=X_combined, current_data=X_current)
+data_quality_test_suite.save_html('./monitoring/reports/data_quality_test_suite.html')
+
+num_target_drift_report = Report(metrics=[
+    TargetDriftPreset(),
+])
+num_target_drift_report.run(reference_data=y_combined, current_data=y_current)
+num_target_drift_report.save_html('./monitoring/reports/num_target_drift_report.html')
--- src/monitoring/evidently.py
+++ src/monitoring/evidently.py
@@ -1,11 +1,11 @@
 from evidently import ColumnMapping
 from evidently.report import Report
 from evidently.metrics.base_metric import generate_column_metrics
-from evidently.metric_preset import DataDriftPreset, TargetDriftPreset
+from evidently.metric_preset import DataDriftPreset, TargetDriftPreset, DataQualityPreset
 from evidently.metrics import *
 from evidently.test_suite import TestSuite
 from evidently.tests.base_test import generate_column_tests
-from evidently.test_preset import DataStabilityTestPreset, NoTargetPerformanceTestPreset, DataDriftTestPreset
+from evidently.test_preset import DataStabilityTestPreset, NoTargetPerformanceTestPreset, DataDriftTestPreset, DataQualityTestPreset
 from evidently.tests import *
 
 def setDataDriftPreset(report):
